1abf3476f41a6088e6a748627aa3970e4f776a02
crypto_yurright <148733325+yurright@users.noreply.github.com>
2024-05-04 17:49:30 +0900

    Update 1929-01-Concatenation-of-Array.js

--- leetcode/1929-01-Concatenation-of-Array.js
+++ leetcode/1929-01-Concatenation-of-Array.js
@@ -1,12 +1,19 @@
 /**
 * Problem: 1929. Concatenation of Array
+* Solution : Using Spread Syntax
+*/
+
+/**
+*Description:
 * Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
 * Specifically, ans is the concatenation of two nums arrays.
 * Return the array ans.
 */
 
 /**
-* Solution : Using Spread Syntax
+* Result
+* Runtime: 53 mx
+* Memory: 51.93 MB
 */
 
 /**
